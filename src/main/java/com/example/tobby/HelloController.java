3e13321fe7fa20114208e7ef24a4a8492dbc3559
conny <jmg41490400@gmail.com>
2023-01-30 18:03:26 +0900

    DispatcherServlet 전환, 스프링컨테이너에 통합

--- src/main/java/com/example/tobby/HelloController.java
+++ src/main/java/com/example/tobby/HelloController.java
@@ -1,7 +1,11 @@
 package com.example.tobby;
 
 import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/hello")
 public class HelloController {
   private final HelloService helloService;
 
@@ -9,6 +13,8 @@ public class HelloController {
     this.helloService = helloService;
   }
 
+  @GetMapping()
+  @ResponseBody
   public String hello(String name) {
     return helloService.sayHello(Objects.requireNonNull(name));
   }
