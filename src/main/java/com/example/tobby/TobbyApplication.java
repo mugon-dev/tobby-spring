ad50d2e03eacee4e6096c7f216260563537e56c5
conny <jmg41490400@gmail.com>
2023-01-30 18:10:45 +0900

    자바 코드 구성 정보 사용하여 컨테이너 구성

--- src/main/java/com/example/tobby/TobbyApplication.java
+++ src/main/java/com/example/tobby/TobbyApplication.java
@@ -3,13 +3,27 @@ package com.example.tobby;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
+@Configuration
 public class TobbyApplication {
 
+  @Bean
+  public HelloController helloController(HelloService helloService) {
+    return new HelloController(helloService);
+  }
+
+  @Bean
+  public HelloService helloService() {
+    return new SimpleHelloService();
+  }
+
   public static void main(String[] args) {
-    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+    AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
       @Override
       protected void onRefresh() {
         super.onRefresh();
@@ -22,11 +36,7 @@ public class TobbyApplication {
         webServer.start();
       }
     };
-    applicationContext.registerBean(HelloController.class);
-    applicationContext.registerBean(SimpleHelloService.class);
+    applicationContext.register(TobbyApplication.class);
     applicationContext.refresh();
-
-
   }
-
 }
