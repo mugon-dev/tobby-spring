70320db4a98d1bf72018f58845a63e78617e4bf0
conny <jmg41490400@gmail.com>
2023-02-05 16:17:00 +0900

    합성 애노테이션 적용

--- src/main/java/com/example/tobby/TobbyApplication.java
+++ src/main/java/com/example/tobby/TobbyApplication.java
@@ -1,28 +1,11 @@
 package com.example.tobby;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-@Configuration
-@ComponentScan // @Component 붙은 Class 찾아 Bean 등록
+
+@MySpringBootAnnotation
 public class TobbyApplication {
 
-  @Bean ServletWebServerFactory servletWebServerFactory(){
-    return new TomcatServletWebServerFactory();
-  }
-  @Bean
-  public DispatcherServlet dispatcherServlet(){
-    // ApplicationContextAware 인터페이스를 받아 구현했기때문에 Bean으로 등록되면 spring container에 등록해줌
-    return new DispatcherServlet();
-  }
-
   public static void main(String[] args) {
-    SpringApplication.run(TobbyApplication.class,  args);
+    SpringApplication.run(TobbyApplication.class, args);
   }
 }
