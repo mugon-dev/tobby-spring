0270db24f84fde3aa286cdd647758e683df734a1
conny <jmg41490400@gmail.com>
2023-02-05 16:10:20 +0900

    메타 애노테이션, 합선 애노테이션

--- src/test/java/com/example/tobby/HelloServiceTest.java
+++ src/test/java/com/example/tobby/HelloServiceTest.java
@@ -3,12 +3,21 @@ package com.example.tobby;
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Test
+@interface UnitTest{
 
+}
 public class HelloServiceTest {
 
-  @Test
+  @UnitTest
   void simpleHelloService() {
     SimpleHelloService helloService = new SimpleHelloService();
     String ret = helloService.sayHello("Test");
