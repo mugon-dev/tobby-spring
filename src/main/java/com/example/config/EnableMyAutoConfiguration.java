a0164b3deef58bb9ca3ca869272747bedf04fb43
conny <jmg41490400@gmail.com>
2023-02-06 13:56:59 +0900

    동적인 자동 구성정보 등록

--- src/main/java/com/example/config/EnableMyAutoConfiguration.java
+++ src/main/java/com/example/config/EnableMyAutoConfiguration.java
@@ -1,7 +1,5 @@
 package com.example.config;
 
-import com.example.config.autoconfig.DispatcherServletConfig;
-import com.example.config.autoconfig.TomcatWebServerConfig;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,7 +8,7 @@ import org.springframework.context.annotation.Import;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+@Import(MyAutoConfigImportSelector.class)
 public @interface EnableMyAutoConfiguration {
 
 }
