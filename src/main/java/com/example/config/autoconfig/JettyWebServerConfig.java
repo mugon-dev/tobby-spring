4522576a6c24b2586a7547e0b7a2103a9ad2c4ff
conny <jmg41490400@gmail.com>
2023-02-07 15:15:45 +0900

    자동구성 정보 대체하기

--- src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package com.example.config.autoconfig;
 
 import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,8 @@ import org.springframework.context.annotation.Bean;
 public class JettyWebServerConfig {
 
   @Bean("jettyWebServerFactory")
+  @ConditionalOnMissingBean
+    // 사용자 정의된 빈이 없으면 사용
     // bean name 지정
   ServletWebServerFactory servletWebServerFactory() {
     return new JettyServletWebServerFactory();
--- src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package com.example.config.autoconfig;
 
 import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,8 @@ import org.springframework.context.annotation.Bean;
 public class TomcatWebServerConfig {
 
   @Bean("tomcatWebServerFactory")
+  @ConditionalOnMissingBean
+    // 사용자 정의된 빈이 없으면 사용
   ServletWebServerFactory servletWebServerFactory() {
     return new TomcatServletWebServerFactory();
   }
