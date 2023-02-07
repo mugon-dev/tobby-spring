79ad7740ec63523cd26fecf6aa06bcaaabaf2921
conny <jmg41490400@gmail.com>
2023-02-07 22:13:37 +0900

    @Value와 PropertySourcesPlaceholderConfigurer

---
+++ src/main/java/com/example/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,15 @@
+package com.example.config.autoconfig;
+
+import com.example.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+
+  @Bean
+  PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+    return new PropertySourcesPlaceholderConfigurer();
+  }
+
+}
--- src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
@@ -2,23 +2,26 @@ package com.example.config.autoconfig;
 
 import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 //@Conditional(TomcatCondition.class)
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
+  @Value("${contextPath}")
+  String contextPath;
+
   @Bean("tomcatWebServerFactory")
   @ConditionalOnMissingBean
     // 사용자 정의된 빈이 없으면 사용
-  ServletWebServerFactory servletWebServerFactory(Environment env) {
+  ServletWebServerFactory servletWebServerFactory() {
     TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-    serverFactory.setContextPath(env.getProperty("contextPath"));
+    serverFactory.setContextPath(this.contextPath);
     return serverFactory;
   }
 
