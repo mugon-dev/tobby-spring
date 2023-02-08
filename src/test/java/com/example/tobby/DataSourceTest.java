1cc53cdd9bba19ade917d6e7bf0abd3192b79c5a
conny <jmg41490400@gmail.com>
2023-02-08 13:42:23 +0900

    JdbcTemplate, 트랜잭션 매니저 구성

--- src/test/java/com/example/tobby/DataSourceTest.java
+++ src/test/java/com/example/tobby/DataSourceTest.java
@@ -4,17 +4,11 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = TobbyApplication.class)
-@TestPropertySource("classpath:/application.properties")
+@HellobootTest
 public class DataSourceTest {
-  
+
   @Autowired
   DataSource dataSource;
 
